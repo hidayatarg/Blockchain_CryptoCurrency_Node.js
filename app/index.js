e451bc0e36b2bd68160c2f54c9138ac3849ce6c4
Hidayat arghandabi <hidayatarg@gmail.com>
2018-05-15 00:55:58 +0300

    Block chain -POST method

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+//add body paser
+const bodyparser= require ('body-parser')
+
 const Blockchain = require('../blockchain');
 
 
@@ -7,8 +10,20 @@ const HTTP_PORT = process.env.HTTP_PORT || 2001;
 const app = express();
 const bc = new Blockchain();
 
+//Json within post request
+app.use(bodyparser.json());
+
 app.get('/blocks', (req, res) => {
 	res.json(bc.chain);
+});
+
+//Post Request
+app.post('/mine', (req, res) => {
+   const block =bc.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+
+    //redirect to the 
+    res.redirect('/block')
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
